2d5dfbc6dc777c965ce74791815810de5658b1b0
Justin Weigle <jw6858@cs.ship.edu>
2019-09-18 11:38:38 -0400

    Made good progress

--- HW/ASTAR/weigle-astar.py
+++ HW/ASTAR/weigle-astar.py
@@ -1,19 +1,38 @@
 #-------------------------------------------#
 #                 HW #2                     #
 #       Author: Justin Weigle               #
-#       Edited: 15 Sept 2019                #
+#       Edited: 18 Sept 2019                #
 #-------------------------------------------#
 #           A* Search Algorithm             #
 #-------------------------------------------#
 
 import csv
 from queue import PriorityQueue
+import math
 
 
 def astar(graph, start, goal, h):
     open_set = PriorityQueue()
     open_set.put(start)
-    came_from = set()
+    came_from = []
+    g_score = [float("inf")] * len(graph)
+    for i in range(1, len(graph)):
+        if graph[i][0] == start:
+            g_score[i] = 0
+    f_score = [math.inf] * len(h)
+    f_score[int(start)] = h[int(start)]
+
+    current = math.inf
+    while open_set:
+        for node in open_set.queue:
+            if int(node) < float(current):
+                current = node
+        if current == goal:
+            return "Goal Reached"
+
+        open_set.get()
+
+
 
 if __name__=="__main__":
     fn = "EdgeWeights.csv"
@@ -32,4 +51,8 @@ if __name__=="__main__":
     for row in reader:
         mincosts.append(row)
 
-    astar(edgeweights, sn, goal, mincosts)
+    sn = "5"
+    goal = "10"
+
+    result = astar(edgeweights, sn, goal, mincosts)
+    print(result)
