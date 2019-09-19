e9589e051a2e349000adfa60fff0ea02307bd31c
Justin Weigle <jw6858@cs.ship.edu>
2019-09-18 22:20:35 -0400

    Changed to using default dict to simplify addressing repeated nodes in EdgeWeights.csv

--- HW/ASTAR/weigle-astar.py
+++ HW/ASTAR/weigle-astar.py
@@ -9,47 +9,48 @@
 import csv
 from queue import PriorityQueue
 import math
-import numpy as np
-
+from collections import defaultdict
 
 def astar(edgeweights, start, goal, h):
     open_set = PriorityQueue()
+    closed_set = []
     open_set.put(start)
-    closed_set = PriorityQueue()
-    came_from = []
-    g_score = [math.inf] * 202
-    g_score[int(start)] = 0
-    f_score = [math.inf] * len(h)
-    f_score[int(start)] = h[int(start)]
 
-    current = math.inf
-    while open_set:
-        for node in open_set.queue:
-            if int(node) < float(current):
-                current = node
-        if current == goal:
-            return "Goal Reached"
-
-        open_set.get(current)
-        closed_set.put(current)
-
+    print(edgeweights[start])
+    for path in edgeweights[start]:
+        if(int(path[0]) < int(goal)):
+            f_score = float(path[1]) + float(h[path[0]][0][int(goal)-1])
+            print(h[path[0]][0][int(goal)-1])
+        else:
+            f_score = float(path[1]) + float(h[goal][0][int(path[0])-1])
+        print(f_score)
 
 if __name__=="__main__":
     fn = "EdgeWeights.csv"
 
     f = open(fn, 'r')
     reader = csv.reader(f)
-    edgeweights = []
+    edgeweights = defaultdict(list)
     for row in reader:
-        edgeweights.append(row)
+        if row:
+            edgeweights[row[0]].append([row[1], row[2]])
+    f.close()
+    """
+    print(edgeweights['1']) #prints the whole set of lists tied to 1
+    print(edgeweights['1'][0]) #prints the list tied to 1 at 0
+    print(edgeweights['1'][0][0]) #prints the 0th element of ^
+    """
 
     fn = "minCosts.csv"
 
     f = open(fn, 'r')
     reader = csv.reader(f)
-    mincosts = []
+    #mincosts = []
+    mincosts = defaultdict(list)
     for row in reader:
-        mincosts.append(row)
+        if row:
+            mincosts[row[0]].append(row[1:])
+    f.close()
 
     sn = "5"
     goal = "10"
