259724c24e76eaf599be59be351dead38eb1c748
Justin Weigle <jw6858@cs.ship.edu>
2019-10-01 22:47:03 -0400

    at evaluation stage

--- HW/GENALG/main.py
+++ HW/GENALG/main.py
@@ -7,35 +7,51 @@
 #---------------------------------------#
 
 import random
-from math import cos, sin, pi
-from math import degrees as deg
+from math import cos, sin, pi, hypot, inf
 
-def fitness(playground, loc, alpha):
-    new_loc = [loc[0] + deg(cos(alpha)), loc[1] + deg(sin(alpha))]
+"""
+define the distance formula for determining the distance of a snake at a
+location 'loc' from the goal
+"""
+dist = lambda loc, goal : hypot(goal[0] - loc[0], goal[1] - loc[1])
 
-class snake ():
-    def __init__(self, playground, start, goal, max_steps):
+class Snake ():
+    def __init__(self, start, max_steps):
         self.alphas = []
         for i in range(max_steps):
-            self.alphas.append(random.uniform(0, (pi/2)))
-        self.bounds = playground
-        self.position = start
-        self.food = goal
+            self.alphas.append([random.uniform(0, pi), random.uniform(0, (pi/2))])
+        self.loc = start
         self.path = []
+        self.eval = inf
+        self.select_prob = 0
 
-    def hunt():
+    def hunt(self):
+        #if the path already exists, reset it
+        if self.path:
+            self.loc = start
+            self.path = []
         for i in range(max_steps):
-            self.path.append(fitness(bounds, position, alphas[i]))
+            self.loc = [
+                    self.loc[0] + cos(self.alphas[i][0]), 
+                    self.loc[1] + sin(self.alphas[i][1])
+            ]
+            self.path.append(self.loc)
 
-
-def gen_snakes(playground, start, goal, opts):
+def gen_snakes(start, opts):
     snakes = []
     for i in range(opts["PopulationSize"]):
-        snakes.append(snake(playground, start, goal, opts["MaxSteps"]))
+        snakes.append(Snake(start, opts["MaxSteps"]))
 
     return snakes
 
+def evaluate(snake, goal, playground):
+    distances = []
+    for coord in snake.path:
+        distances.append(dist(coord, goal))
+    print(distances[24])
+
 if __name__=="__main__":
+    random.seed(3295)
     # set things up
     playground = [(0,32), (0,18)]
 
@@ -51,7 +67,13 @@ if __name__=="__main__":
         "MaxSteps":25,
     })
 
-    snakes = gen_snakes(playground, start, goal, opts)
-#    print(snakes[49].alphas)
+    snakes = gen_snakes(start, opts)
 
+#    snake0_hunt = snakes[0].hunt()
+#    print(snake0_hunt)
 
+    for snake in snakes:
+        snake.hunt()
+
+    for snake in snakes:
+        evaluate(snake, goal, playground)
